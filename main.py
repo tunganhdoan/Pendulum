f5794d2e0fd791071a049ee01e2350f45abca39a
doantunganh <tunganh.doan@lqdtu.edu.vn>
2022-07-04 14:59:36 +0700

    Set root.state('zoomed') + icon + geometry

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ from tkinter import Tk
 
 if __name__ == '__main__':
     root = Tk()
+    root.state('zoomed')
     root.title("Pendulum Simulation (ver:1.0)")
-    root_width = 600
-    root_height = 400
-    # get the screen width and height using wininfo_width() and wininfo_height()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    # find the center point
-    center_x = int(screen_width / 2 - root_width / 2)
-    center_y = int(screen_height / 2 - root_height / 2)
-    # set the position of the window to the center of the screen
-    root.geometry(f'{root_width}x{root_height}+{center_x}+{center_y}')
+    root.geometry('1360x768+300+20')
+    root.iconbitmap('./assets/pd.ico')
     root.mainloop()
