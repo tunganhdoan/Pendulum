1e8c9e720a3616028f4e34666faae805a5027a56
doantunganh <tunganh.doan@lqdtu.edu.vn>
2022-07-04 23:43:48 +0700

    Create DoubleVar() variables to bind values of scale and spinbox

--- main_frame.py
+++ main_frame.py
@@ -8,17 +8,20 @@ class MainFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
         options = {'padx': 5, 'pady': 5}
+        length = tk.DoubleVar()
         length_label = ttk.Label(self, text="Length").grid(column=0, row=0, sticky=tk.E)
-        length_scale = ttk.Scale(self, variable=input, orient='horizontal', length=200).grid(column=1, row=0)
-        length_spinbox = ttk.Spinbox(self, textvariable=input, wrap=True, width=10).grid(column=2, row=0)
+        length_scale = ttk.Scale(self, variable=length, orient='horizontal', length=200).grid(column=1, row=0)
+        length_spinbox = ttk.Spinbox(self, textvariable=length, wrap=True, width=10).grid(column=2, row=0)
 
+        mass = tk.DoubleVar()
         mass_label = ttk.Label(self, text="Mass").grid(column=0, row=1, sticky=tk.E)
-        mass_scale = ttk.Scale(self, variable=input, orient='horizontal', length=200).grid(column=1, row=1)
-        mass_spinbox = ttk.Spinbox(self, textvariable=input, wrap=True, width=10).grid(column=2, row=1)
+        mass_scale = ttk.Scale(self, variable=mass, orient='horizontal', length=200).grid(column=1, row=1)
+        mass_spinbox = ttk.Spinbox(self, textvariable=mass, wrap=True, width=10).grid(column=2, row=1)
 
+        gravity = tk.DoubleVar()
         gravity_label = ttk.Label(self, text="Gravity").grid(column=0, row=2, sticky=tk.E)
-        gravity_scale = ttk.Scale(self, variable=input, orient='horizontal', length=200).grid(column=1, row=2)
-        gravity_spinbox = ttk.Spinbox(self, textvariable=input, wrap=True, width=10).grid(column=2, row=2)
+        gravity_scale = ttk.Scale(self, variable=gravity, orient='horizontal', length=200).grid(column=1, row=2)
+        gravity_spinbox = ttk.Spinbox(self, textvariable=gravity, wrap=True, width=10).grid(column=2, row=2)
 
         initial_angle_label = ttk.Label(self, text="Initial Angle").grid(column=0, row=3, sticky=tk.E)
         initial_angle_scale = ttk.Scale(self, variable=input, orient='horizontal', length=200).grid(column=1, row=3)
