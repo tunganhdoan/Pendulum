e8cabefb54ef05cfc875641bf01fe8960cd2ace3
doantunganh <tunganh.doan@lqdtu.edu.vn>
2022-07-04 20:46:20 +0700

    Add grid to main_frame.py; learn how to use columnfigure(index, weight)

--- main_frame.py
+++ main_frame.py
@@ -1,12 +1,20 @@
 from tkinter import ttk
-
+import tkinter as tk
 
 class MainFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
         options = {'padx': 5, 'pady': 5}
+        # config the grid
+        # configure the grid
+        self.columnconfigure(0, weight=3)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, weight=1)
         # label
-        self.label = ttk.Label(self, text='Hello, Tkinter!')
-        self.label.pack(**options)
+        # username
+        length_label = ttk.Label(self, text="Length:")
+        length_label.grid(column=1, row=0, sticky=tk.W, **options)
+
         # show the frame on the container
         self.pack(**options)
