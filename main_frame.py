cd2ad5c88109c759055f19949f4a330ca65a1f19
doantunganh <tunganh.doan@lqdtu.edu.vn>
2022-07-05 10:04:41 +0700

    get and assign variables in renew_calculation

--- main_frame.py
+++ main_frame.py
@@ -9,8 +9,6 @@ class MainFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
         options = {'padx': 5, 'pady': 5}
-        length = tk.DoubleVar()
-        length.set(1.0)
 
         def length_slider_changed(event):
             renew_calculation()
@@ -18,10 +16,25 @@ class MainFrame(ttk.Frame):
         def gravity_slider_changed(event):
             renew_calculation()
 
-        def renew_calculation():
-            tharm = 2 * np.pi * np.sqrt(length.get() / gravity.get())
-            result.configure(text=str(tharm))
+        def mass_slider_changed(event):
+            renew_calculation()
 
+        def initial_angle_slider_changed(event):
+            renew_calculation()
+
+        def time_step_slider_changed(event):
+            renew_calculation()
+
+        def renew_calculation():
+            l = length.get()
+            m = mass.get()
+            g = gravity.get()
+            theta = initial_angle.get()
+            dt = time_step.get()
+            tharm = 2 * np.pi * np.sqrt(length.get() / gravity.get())
+
+        length = tk.DoubleVar()
+        length.set(1.0)
         length_label = ttk.Label(self, text="Length").grid(column=0, row=0, sticky=tk.E)
         length_scale = ttk.Scale(self, variable=length, orient='horizontal', length=200, from_=1, to=50,
                                  command=length_slider_changed).grid(column=1, row=0)
@@ -29,25 +42,36 @@ class MainFrame(ttk.Frame):
                                                                                                           row=0)
 
         mass = tk.DoubleVar()
+        mass.set(2)
         mass_label = ttk.Label(self, text="Mass").grid(column=0, row=1, sticky=tk.E)
-        mass_scale = ttk.Scale(self, variable=mass, orient='horizontal', length=200).grid(column=1, row=1)
-        mass_spinbox = ttk.Spinbox(self, textvariable=mass, wrap=True, width=10).grid(column=2, row=1)
+        mass_scale = ttk.Scale(self, variable=mass, orient='horizontal', length=200, from_=1, to=20,
+                                  command=mass_slider_changed).grid(column=1, row=1)
+        mass_spinbox = ttk.Spinbox(self, textvariable=mass, wrap=True, width=10, from_=1, to=20,
+                                      increment=0.1).grid(column=2,
+                                                          row=1)
 
         gravity = tk.DoubleVar()
         gravity.set(9.8)
         gravity_label = ttk.Label(self, text="Gravity").grid(column=0, row=2, sticky=tk.E)
         gravity_scale = ttk.Scale(self, variable=gravity, orient='horizontal', length=200, from_=1, to=20,
                                   command=gravity_slider_changed).grid(column=1, row=2)
-        gravity_spinbox = ttk.Spinbox(self, textvariable=gravity, wrap=True, width=10, from_=1, to=20).grid(column=2,
-                                                                                                            row=2)
-
+        gravity_spinbox = ttk.Spinbox(self, textvariable=gravity, wrap=True, width=10, from_=1, to=20,
+                                      increment=0.1).grid(column=2,
+                                                          row=2)
+        initial_angle = tk.DoubleVar()
+        initial_angle.set(20.0)
         initial_angle_label = ttk.Label(self, text="Initial Angle").grid(column=0, row=3, sticky=tk.E)
-        initial_angle_scale = ttk.Scale(self, variable=input, orient='horizontal', length=200).grid(column=1, row=3)
-        initial_angle_spinbox = ttk.Spinbox(self, textvariable=input, wrap=True, width=10).grid(column=2, row=3)
+        initial_angle_scale = ttk.Scale(self, variable=initial_angle, orient='horizontal', length=200, from_=0, to=360,
+                                        command=initial_angle_slider_changed).grid(column=1, row=3)
+        initial_angle_spinbox = ttk.Spinbox(self, textvariable=initial_angle, wrap=True, width=10, from_=1,
+                                            to=360, increment=1).grid(column=2, row=3)
 
+        time_step = tk.DoubleVar(value=0.05)
         time_step_label = ttk.Label(self, text="Time Step").grid(column=0, row=4, sticky=tk.E)
-        time_step_scale = ttk.Scale(self, variable=input, orient='horizontal', length=200).grid(column=1, row=4)
-        time_step_spinbox = ttk.Spinbox(self, textvariable=input, wrap=True, width=10).grid(column=2, row=4)
+        time_step_scale = ttk.Scale(self, variable=time_step, orient='horizontal', length=200, from_=0.001, to=0.1,
+                                    command=time_step_slider_changed).grid(column=1, row=4)
+        time_step_spinbox = ttk.Spinbox(self, textvariable=time_step, wrap=True, width=10, from_=0.001, to=0.1).grid(
+            column=2, row=4)
 
         # Create a photoimage object of the image in the path
         image1 = Image.open("images/ani_pen.gif")
@@ -77,16 +101,5 @@ class MainFrame(ttk.Frame):
         # link function to change dropdown
         dropdown_value.trace('w', change_dropdown)
 
-        # Results
 
-        result = ttk.Label(self, text="abc")
-        result.grid(column=2, row=10)
-
-        # Initial angular displacement (rad), tangential velocity (m.s-1)
-        theta0, v0 = np.radians(60), 0
-        # Estimate of the period using the harmonic (small displacement) approximation.
-        # The real period will be longer than this.
-        tharm = 2 * np.pi * np.sqrt(length.get() / gravity.get())
-        print("{}".format(tharm))
-        # add padding to the frame and show it
         self.grid(padx=50, pady=50, sticky=tk.NSEW)
