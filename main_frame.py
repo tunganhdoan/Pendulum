6a8ce730278716d0ea114c1c84c09c4043225e42
doantunganh <tunganh.doan@lqdtu.edu.vn>
2022-07-05 10:19:20 +0700

    round up the values of variables

--- main_frame.py
+++ main_frame.py
@@ -11,18 +11,23 @@ class MainFrame(ttk.Frame):
         options = {'padx': 5, 'pady': 5}
 
         def length_slider_changed(event):
+            length.set(round(length.get(),1))
             renew_calculation()
 
         def gravity_slider_changed(event):
+            gravity.set(round(gravity.get(), 1))
             renew_calculation()
 
         def mass_slider_changed(event):
+            mass.set(round(mass.get(), 1))
             renew_calculation()
 
         def initial_angle_slider_changed(event):
+            initial_angle.set(round(initial_angle.get()))
             renew_calculation()
 
         def time_step_slider_changed(event):
+            time_step.set(round(time_step.get(), 3))
             renew_calculation()
 
         def renew_calculation():
@@ -38,11 +43,11 @@ class MainFrame(ttk.Frame):
         length_label = ttk.Label(self, text="Length").grid(column=0, row=0, sticky=tk.E)
         length_scale = ttk.Scale(self, variable=length, orient='horizontal', length=200, from_=1, to=50,
                                  command=length_slider_changed).grid(column=1, row=0)
-        length_spinbox = ttk.Spinbox(self, textvariable=length, wrap=True, width=10, from_=1, to=50).grid(column=2,
+        length_spinbox = ttk.Spinbox(self, textvariable=length, wrap=True, width=10, from_=1, to=50, format="%.2f" ).grid(column=2,
                                                                                                           row=0)
 
         mass = tk.DoubleVar()
-        mass.set(2)
+        mass.set(2.0)
         mass_label = ttk.Label(self, text="Mass").grid(column=0, row=1, sticky=tk.E)
         mass_scale = ttk.Scale(self, variable=mass, orient='horizontal', length=200, from_=1, to=20,
                                   command=mass_slider_changed).grid(column=1, row=1)
@@ -58,8 +63,8 @@ class MainFrame(ttk.Frame):
         gravity_spinbox = ttk.Spinbox(self, textvariable=gravity, wrap=True, width=10, from_=1, to=20,
                                       increment=0.1).grid(column=2,
                                                           row=2)
-        initial_angle = tk.DoubleVar()
-        initial_angle.set(20.0)
+        initial_angle = tk.IntVar()
+        initial_angle.set(20)
         initial_angle_label = ttk.Label(self, text="Initial Angle").grid(column=0, row=3, sticky=tk.E)
         initial_angle_scale = ttk.Scale(self, variable=initial_angle, orient='horizontal', length=200, from_=0, to=360,
                                         command=initial_angle_slider_changed).grid(column=1, row=3)
@@ -70,7 +75,7 @@ class MainFrame(ttk.Frame):
         time_step_label = ttk.Label(self, text="Time Step").grid(column=0, row=4, sticky=tk.E)
         time_step_scale = ttk.Scale(self, variable=time_step, orient='horizontal', length=200, from_=0.001, to=0.1,
                                     command=time_step_slider_changed).grid(column=1, row=4)
-        time_step_spinbox = ttk.Spinbox(self, textvariable=time_step, wrap=True, width=10, from_=0.001, to=0.1).grid(
+        time_step_spinbox = ttk.Spinbox(self, textvariable=time_step, wrap=True, width=10, increment=0.001, from_=0.001, to=0.1).grid(
             column=2, row=4)
 
         # Create a photoimage object of the image in the path
